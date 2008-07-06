a8f71bafaa8969a39d48c16fc5c91e035d06ee80
John Barnette <jbarnette@gmail.com>
2008-07-05 23:33:16 -0700

    Make fixture names a little more canonical.

--- spec/fixtures/campaign.rb
+++ spec/fixtures/campaign.rb
@@ -4,24 +4,26 @@ module Fixtures
       start :incomplete
 
       on :draft do
-        move [:incomplete, :draft, :rejected] => :draft
+        move [:incomplete, :rejected] => :draft
+        stay :draft
       end
 
       on :start do
-        move [:incomplete, :pending_authorization, :draft, :rejected] => :pending_authorization
+        move [:incomplete, :draft, :rejected] => :authorizing
+        stay :authorizing
       end
 
       on :authorize do
-        move :pending_authorization => :pending_approval
-        move :pending_reauthorization => :approved
+        move :authorizing => :approving
+        move :reauthorizing => :approved
       end
 
       on :approve do
-        move :pending_approval => :approved
+        move :approving => :approved
       end
 
       on :accept do
-        move :rejected => :pending_authorization
+        move :rejected => :authorizing
       end
 
       on :traffic do
@@ -37,39 +39,39 @@ module Fixtures
       end
 
       on :change_spend do
-        move :pending_approval => :pending_authorization
-        move [:approved, :trafficked] => :pending_reauthorization
+        move :approving => :authorizing
+        move [:approved, :trafficked] => :reauthorizing
 
         stay :draft, :incomplete, :rejected
       end
 
-      on :update_theme do
-        stay :draft, :incomplete, :pending_approval, 
+      on :update_widget do
+        stay :draft, :incomplete, :approving, 
           :approved, :trafficked, :rejected      
       end
 
-      on :delete_theme do
+      on :delete_widget do
         move :trafficked => :approved
-        stay :draft, :incomplete, :pending_approval, :approved, :rejected
+        stay :draft, :incomplete, :approving, :approved, :rejected
       end
 
-      on :approve_themes do 
+      on :approve_widgets do 
         move :trafficked => :approved
-        stay :pending_approval, :approved
+        stay :approving, :approved
       end
 
       on :change_trafficking do
         move :trafficked => :approved
-        stay :incomplete, :draft, :approved, :pending_approval, :rejected
+        stay :incomplete, :draft, :approved, :approving, :rejected
       end
 
       on :hide do
-        move [:draft, :rejected, :approved, :pending_approval, :trafficked] => :hidden
+        move [:draft, :rejected, :approved, :approving, :trafficked] => :hidden
         stay :hidden
       end
 
       on :reject do
-        move [:pending_approval, :trafficked, :approved] => :rejected
+        move [:approving, :trafficked, :approved] => :rejected
       end
     end
   end
